38e67c6e06d0fdff18fd9fecf6d9f63cef388120
Adilson Mandlate <adilsonmandlate@gmail.com>
2022-05-12 20:18:43 +0200

    feat: setup api routes

---
+++ src/infrastructure/config/routes.js
@@ -0,0 +1,9 @@
+const router = require('express').Router()
+const FastGlob = require('fast-glob')
+
+module.exports = (app) => {
+  app.use('/api', router)
+  FastGlob.sync('**/src/infrastructure/routes/**/*.js').forEach((file) =>
+    require(`../../../${file}`)(router)
+  )
+}
