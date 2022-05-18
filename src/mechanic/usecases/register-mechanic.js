12cc6ab3dbdc093313a0d3b9ec14766c5b576e39
Adilson Mandlate <adilsonmandlate@gmail.com>
2022-05-18 10:52:22 +0200

    feat: use mechanic entity to create new mechanic data

--- src/mechanic/usecases/register-mechanic.js
+++ src/mechanic/usecases/register-mechanic.js
@@ -1,10 +1,13 @@
+const Mechanic = require('../domain/Mechanic')
+
 class RegisterMechanicUseCase {
   constructor({ MechanicRepository }) {
     this.MechanicRepository = MechanicRepository
   }
 
   async handle(mechanicData) {
-    const mechanic = await this.MechanicRepository.add(mechanicData)
+    const newMechanic = new Mechanic(mechanicData)
+    const mechanic = await this.MechanicRepository.add(newMechanic)
 
     return mechanic
   }
