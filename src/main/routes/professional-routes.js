0869236fb1a93007bbe0cf283180de900bf31003
Adilson Mandlate <adilsonmandlate@gmail.com>
2022-07-14 08:38:42 +0200

    chore: add route for professional search by location

--- src/main/routes/professional-routes.js
+++ src/main/routes/professional-routes.js
@@ -1,9 +1,14 @@
 const { adapt } = require('../adapters/express-router-adapter')
 const GetProfessionalByIdComposer = require('../factories/GetProfessionalByIdComposer')
+const GetProfessionalByLocationComposer = require('../factories/GetProfessionalByLocationComposer')
 const RegisterProfessionalComposer = require('../factories/RegisterProfessionalComposer')
 
 module.exports = (router) => {
   router.get('/professionals/:id', adapt(GetProfessionalByIdComposer.compose()))
+  router.get(
+    '/professionals/:latitude/:longitude',
+    adapt(GetProfessionalByLocationComposer.compose())
+  )
   router.post(
     '/professionals/new',
     adapt(RegisterProfessionalComposer.compose())
