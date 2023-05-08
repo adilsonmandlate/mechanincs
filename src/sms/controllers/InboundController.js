d98645c429fc76c6e46c042062c76e549a2f241a
Adilson Mandlate <adilsonmandlate@gmail.com>
2023-05-08 16:00:18 +0200

    feat: reply the messages based on professional feedback

--- src/sms/controllers/InboundController.js
+++ src/sms/controllers/InboundController.js
@@ -1,15 +1,36 @@
 const { MessagingResponse } = require('twilio').twiml
+const { emitter } = require('../../utils/eventEmiter')
 
 const InboundController = (req, res) => {
+  let responseText
   const twiml = new MessagingResponse()
+  const message = req.body.Body
 
-  /**
-   * We should now check who sent the message and do what we are supposed to next.
-   * Verificar como enviar os detalhes do cliente e do profissional para cada um deles.
-   * And thats it.
-   */
+  switch (message?.trim()) {
+    case '1':
+    case 'sim':
+    case 'yes':
+    case 'aceitar':
+    case 'aceite':
+      responseText =
+        'Confirmado. Enviamos dentro de instantes os detalhes do teu cliente.'
+      emitter.emit('trabalho:aceite', req.body.From)
+      break
+    case '0':
+    case 'nao':
+    case 'no':
+    case 'negar':
+    case 'recusado':
+    case 'recusar':
+      responseText = 'Obrigado.'
+      emitter.emit('trabalho:recusado', req.body.From)
+      break
+    default:
+      responseText =
+        'Por favor, responda com 1 para aceitar ou com 0 para recusar.'
+  }
 
-  twiml.message('Confirmado. Enviamos em breve os detalhes do teu cliente.')
+  twiml.message(responseText)
   return res.type('text/xml').send(twiml.toString())
 }
 
