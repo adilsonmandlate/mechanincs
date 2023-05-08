38d62d3a00531cf232e1d72ff83694a83b0ff0ff
Adilson Mandlate <adilsonmandlate@gmail.com>
2023-05-08 15:54:52 +0200

    chore: add sms module

--- src/main/config/sms.js
+++ src/main/config/sms.js
@@ -1,5 +1,9 @@
-const sid = 'AC54341ac24887cb515e072dc9fb5ac9bc'
-const token = '62b6173996a3b88a70ecece085a518d7'
-const twilio = require('twilio')(sid, token)
+const sid = process.env.SID
+const token = process.env.TOKEN
+const fromNumber = process.env.SMS_FROM_NUMBER
+const service = require('twilio')(sid, token)
 
-module.exports = twilio
+module.exports = {
+  service,
+  fromNumber
+}
